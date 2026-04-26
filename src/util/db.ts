c84dac3371254a7a938c9bb47d9bb99975932ed8
lefteris797 <lefteris797@gmail.com>
2026-04-26 04:15:12 +0300

    feat db.ts correction

--- src/util/db.ts
+++ src/util/db.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async() => {
-    try{
+     try {
+        if (!MONGO_URI) {
+            throw new Error("MONGO_URI is not defined");
+        }
         await mongoose.connect(MONGO_URI);
         console.log("MongoDB Connected");
     } catch (err){
